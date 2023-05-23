e8ef1598d31c49975596917b9f79dcc42c01bdd9
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-05-23 17:03:53 +0200

    stamp json

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css' integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==' crossorigin='anonymous'/>
     <link rel="stylesheet" href="style.css">
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <title>Document</title>
 </head>
@@ -22,17 +20,14 @@
     <div id="app">
     <div class="container col-3 bg-secondary py-2 my-5">
       <h1>TO DO LIST</h1>
-      <div class="tasks d-flex">
-        <ul>
-          <li>task 1<i class="fa-solid fa-trash-can"></i></li>
-          <li>task 2<i class="fa-solid fa-trash-can"></i></li>
-          <li>task 3<i class="fa-solid fa-trash-can"></i></li>
-          <li>task 4<i class="fa-solid fa-trash-can"></i></li>
+      <div class="tasks">
+        <ul v-for="item in list">
+          <li>{{ item.text }}<i class="fa-solid fa-trash-can"></i></li>
         </ul>
       </div>
       <div class="add-tasks">
         <input type="text" name="newTask" placeholder="Inserisci nuova task">
-        <button type="button" class="btn btn-primary">Add Task</button>
+        <button type="button" class="btn btn-primary mx-3">Add Task</button>
       </div>
     </div>
     </div>
--- main.js
+++ main.js
@@ -2,13 +2,27 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-
+      apiUrl: 'http://localhost/php-todo-list-json/server.php',
+      list: []
     }
   },
 
   methods:{
+    stampList(){
+      console.log('ciaooo');
+      console.log(axios);
+      axios.post(this.apiUrl).then(result => {
+        this.list = result.data;
+        console.log(this.list)
+      })
+    }
 
   },
 
-  
+  mounted(){
+      this.stampList();
+
+  },
+
+
 }).mount('#app')
